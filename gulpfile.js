b84570823aad3bc274920083059c63a386034ee9
gurumohanreddy <gurumohan449@gmail.com>
2015-11-09 12:24:44 +0530

    basic form completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+
+gulp.task('sass', function () {
+  gulp.src('./development/assets/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./production/css/'));
 });
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./development/assets/scss/*.scss', ['sass']);
+});
+
+gulp.task('default',['sass:watch']);
---
+++ production/js/app.js
@@ -0,0 +1,9 @@
+angular.module("Etchpost",[]);
+
+angular.module("Etchpost")
+    .controller("postctrl",function(){
+      var postctrl = this;
+      postctrl.title ="Etch Bricks";
+      
+
+    });
