e1ab660087c18517dab73278794219ca1bf873d1
gurumohanreddy <gurumohan449@gmail.com>
2015-11-09 17:02:19 +0530

    fetching the parse data

--- production/js/app.js
+++ production/js/app.js
@@ -1,21 +1,61 @@
 angular.module("Etchpost",[]);
 
 angular.module("Etchpost")
-    .controller("Postctrl",function(){
+    .controller("Postctrl",function($q){
       var postctrl = this;
+      Parse.initialize("OtJJlIClBwmB2KgUmYw2G8IjkoSCHNbRac7V4nMB", "UEonv2bU7qauUguVNZetrKA49uR1NsGf5Ilc7IGW");
+        var Post = Parse.Object.extend("Post");
 
       postctrl.addPost = function(){
-        console.log(postctrl.newPost);
         postctrl.posts.push(postctrl.newPost);
+        var post = new Post();
+        post.set("title",postctrl.newPost.title);
+        post.set("description",postctrl.newPost.description);
+      post.save({
+      success: function(object) {
+        console.log("Post saved sucessfully");
+      },
+      error: function(model, error) {
+        console.log("Error "+ error.message);
+      }
+      });
+
         postctrl.initializeNewPost();
-        console.log(postctrl.posts);
+
       };
+
+      function fetchPost(){
+          var query = new Parse.Query(Post);
+          var differedQuery = $q.defer();
+          query.find().then(function(data){
+            differedQuery.resolve(data);
+          },function(error){
+            differedQuery.reject(data);
+          });
+          differedQuery.promise
+          .then(function(data){
+              postctrl.posts = [];
+              angular.forEach(data,function(obj){
+                post = {};
+                post.title = obj.get("title");
+                post.description = obj.get("description");
+                postctrl.posts.push(post);
+              });
+          })
+          .catch(function(error){
+              console.log("Error in fetching "+error.message);
+          })
+
+
+      }
 
       postctrl.initializeNewPost = function(){
         postctrl.newPost = {};
       };
 
       postctrl.initializeNewPost();
-      postctrl.posts =[];
+      fetchPost();
+
+
 
     });
